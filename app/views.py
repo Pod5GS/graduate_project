ee6141beab29a735201a060363ed2c00833cc365
Pod5GS <wcincredible@live.com>
2017-04-26 23:36:33 +0800

    modify index.html

--- app/views.py
+++ app/views.py
@@ -20,11 +20,8 @@ def upload():
     dir_path = os.path.join(APP_ROOT, 'static/data')
     upload_image(dir_path, upload_file)
     path = 'static/data/' + upload_file.filename
-    color_map_path = 'static/result/colormap.png'
-    prediction_path = 'static/result/prediction.png'
     parse_image(upload_file.filename)
     return jsonify({
         'path': path,
-        'color_map_path': color_map_path,
-        'prediction_path': prediction_path
+        'name': upload_file.filename
     })
